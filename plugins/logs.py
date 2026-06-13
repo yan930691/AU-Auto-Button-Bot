2d35c8e3460c6382698607a98baab5be7d227526
Coder-yt <animeupdatesau04@gmail.com>
2026-06-13 15:08:35 +0530

    logs.py

--- plugins/logs.py
+++ plugins/logs.py
@@ -1,19 +1,15 @@
 # ------------------------- #
-# Don't Remove Credit 
+# Don't Remove Credit
 # Owner @Mr_Mohammed_29
 # ------------------------- #
 
 from config import LOG_CHANNEL
 
-# ------------------------- #
-# Don't Remove Credit 
-# Owner @Mr_Mohammed_29
-# ------------------------- #
 
-async def send_log(
-    client,
-    text
-):
+async def send_log(client, text):
+
+    if not LOG_CHANNEL:
+        return
 
     try:
 
@@ -22,9 +18,11 @@ async def send_log(
             text
         )
 
-    except:
+    except Exception as e:
 
-        pass
+        print(
+            f"LOG ERROR: {e}"
+        )
 
 # ------------------------- #
 # Don't Remove Credit 
